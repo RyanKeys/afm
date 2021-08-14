4c18d999cbc8186bd8aade3eb068bf6453653df4
Ryan Keys <r.keys1998@gmail.com>
2021-08-14 05:31:56 -0700

    ssl

--- server.js
+++ server.js
@@ -20,22 +20,22 @@ let options = {
 // also okay: https.createServer({cert, ca, key}, (req, res) => { ...
 const httpsServer = https.createServer(options, app);
 
-// TURN OFF IN PRODUCTION
-const allowedOrigins = ["firemap.global"];
-const cors = require("cors");
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// // TURN OFF IN PRODUCTION
+// const allowedOrigins = ["https://firemap.global"];
+// const cors = require("cors");
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg =
+//           "The CORS policy for this site does not allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 app.use("/static", express.static("public"));
 app.use(express.json());
