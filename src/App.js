2b1552626e88c5af04e8abcf0272f40ec136c198
Ryan Keys <r.keys1998@gmail.com>
2021-08-13 22:26:15 -0700

    dynamic routing

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
       });
       return response.json(); // parses JSON response into native JavaScript objects
     }
