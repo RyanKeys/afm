c4ad2b4a0b6fb0654d1f74d960f4cd2e05b976c3
Ryan Keys <r.keys1998@gmail.com>
2021-08-14 05:41:52 -0700

    ssl

--- server.js
+++ server.js
@@ -20,23 +20,6 @@ let options = {
 // also okay: https.createServer({cert, ca, key}, (req, res) => { ...
 const httpsServer = https.createServer(options, app);
 
-// TURN OFF IN PRODUCTION
-const allowedOrigins = ["*"];
-const cors = require("cors");
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
-
 app.use("/static", express.static("public"));
 app.use(express.json());
 app.use(express.static(__dirname));
